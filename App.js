e0959d639b68803e8395442e735f6d853f1bf10b
vinnidias <vinicius.3991@gmail.com>
2020-06-23 01:07:54 -0300

    criação da stack da tela de tarefas

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TelaDeLogin from './src/telas/telaDeLogin'
 import TelaDeCadastro from './src/telas/telaDeCadastro'
-
+import TelaDeTarefas from './src/telas/telaDeTarefas'
 
 const Stack = createStackNavigator()
 
@@ -12,10 +12,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator 
-        initialRouteName='TelaDeCadastro'
+        initialRouteName='TelaDeTarefas'
         screenOptions={{ headerShown: false}}>
         <Stack.Screen name='TelaDeLogin' component={TelaDeLogin}/>
         <Stack.Screen name='TelaDeCadastro' component={TelaDeCadastro}/>
+        <Stack.Screen name='TelaDeTarefas' component={TelaDeTarefas}/>
     </Stack.Navigator>
     </NavigationContainer>
   );
