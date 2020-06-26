95e03b845eb6fa77f069a343465836d63fb6dc5e
vinnidias <vinicius.3991@gmail.com>
2020-06-26 00:15:41 -0300

    botao de icone componentizado

---
+++ src/componentes/BotaoIcone.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+function BotaoIcone({pressionado, icone}){
+    return(
+        <View style={estilos.containerBotoes}>
+				<TouchableOpacity onPress={pressionado}>
+					<Text style={estilos.botaoExcluir}>
+						{icone}
+					</Text>
+				</TouchableOpacity>
+			</View>
+    )
+}
+
+const estilos ={
+    containerBotoes:{
+		flexDirection: 'row',
+		alignSelf: 'flex-end',
+        marginLeft: 10,
+    },
+    botaoExcluir:{
+		alignSelf: 'flex-end',
+		margin: 15,
+		marginLeft: 5
+	}
+}
+
+export default BotaoIcone;
